eb69110402ecb4ceebf369675e57e3bc55ef8200
Lluc <lenrique608@alumnes.politecnicllevant.cat>
2026-04-21 13:48:03 +0200

    S'han afegit a AppParameters apunts de com es podrien afegir objectes per una connexio amb una base de dades i com poder fer un patró singelton.

--- src/main/java/javaTesting/Resources/AppParameters.java
+++ src/main/java/javaTesting/Resources/AppParameters.java
@@ -1,9 +1,35 @@
 package javaTesting.Resources;
 
 public class AppParameters {
+    // afegir-hi objectes connexio de Base de Dades
 
     public static final String DOMAIN = "127.0.0.1";
     public static final String PORT = "80";
+    // public static final Object BD_CON = new ClasseConnexioBBDD();
+    // public static Object MESSAGES_TRANSLATIONS;
+
+    // Implementar SINGELTON PATTERN
+    private static AppParameters instance;
+
+    private AppParameters(){
+
+    }
+
+    private void initApp(){
+        // codi necessari per a la inicialització de l'App.
+    }
+
+    public void resetApp(){
+        // codi per reconfigurar l'App.
+    }
+
+    public AppParameters getInstance(){
+        if (instance == null){
+            instance = new AppParameters();
+        }
+
+        return instance;
+    }
 
     public static String deployedURL(){
         return "https://" + DOMAIN + ":" + PORT;
