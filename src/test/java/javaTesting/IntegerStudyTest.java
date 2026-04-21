d94e7d747b4bfcbac6b5a5a802181f4d8cb27e6f
Lluc <lenrique608@alumnes.politecnicllevant.cat>
2026-04-21 13:00:32 +0200

    assertTrue comparat dos objectes amb ==

--- src/test/java/javaTesting/IntegerStudyTest.java
+++ src/test/java/javaTesting/IntegerStudyTest.java
@@ -3,6 +3,7 @@ package javaTesting;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class IntegerStudyTest {
 
@@ -26,5 +27,7 @@ public class IntegerStudyTest {
         assertEquals("El nombre vuit val realment 8", 8, vuit.intValue());
 
         assertEquals("El nombre vuit val realment 8", 8, eight.intValue());
+
+        assertTrue(vuit == eight);
     }
 }
