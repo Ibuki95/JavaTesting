3dd5cd2b3aa39b2e6306946f9b1c563579a149bd
Lluc <lenrique608@alumnes.politecnicllevant.cat>
2026-04-21 13:33:20 +0200

    S'ha creat una nova classe AppParametersTest amb dues variables del domini i el port esperats més un mètode per comprovar el domini.

---
+++ src/test/java/javaTesting/AppParametersTest.java
@@ -0,0 +1,16 @@
+package javaTesting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppParametersTest {
+
+    public static final String DOMAIN_EXPECTED = "127.0.0.1";
+    public static final String PORT_EXPECTED = "80";
+
+    @Test
+    public void domainURLCorrect(){
+        Assert.assertEquals("Retorna la part FQDN", DOMAIN_EXPECTED, AppParametersTest.DOMAIN_EXPECTED);
+    }
+}
+
