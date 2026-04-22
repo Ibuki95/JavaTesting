a565d0db3987f6b70aee93052a09a1b9972b8bea
Lluc <lucasenriquebarcelo@gmail.com>
2026-04-22 18:43:43 +0200

    S'ha afegit dins User setters de les dues variables.

--- src/main/java/DomainEntities/User.java
+++ src/main/java/DomainEntities/User.java
@@ -14,10 +14,16 @@ public class User {
         this.password = password;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
     public String getName(){
         return name;
     }
 
+    public void setPassword(String password){
+        this.password = password;
+    }
     public String getPassword(){
         return password;
     }
