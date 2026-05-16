35d167a562335486fa6e271f51878b5b1098b80f
Ibuki95 <lucasenriquebarcelo@gmail.com>
2026-05-16 15:45:23 +0200

    S'ha creat dos nous mètodes, un amb 3 arraylist i un altre per rellenar un arraylist amb els 5 dies de la setmana.

--- src/test/java/javaTesting/collectionsTest/SimpleCollectionTest.java
+++ src/test/java/javaTesting/collectionsTest/SimpleCollectionTest.java
@@ -19,10 +19,31 @@ public class SimpleCollectionTest {
             numerals.add(s);
         }
 
-        assertTrue(numerals.size() == 4);
+        numerals.add("quatre");
+
+        assertTrue(numerals.size() == 5);
 
         List<String> llistaNumeros = Arrays.asList(nombres);
 
         assertTrue(llistaNumeros.size() == 4);
+    }
+
+    @Test
+    public void diesSetmana(){
+        List<String> diesSetmana;
+        List<String> diesFeiners = new ArrayList<>();
+        List<String> diesCapDeSetmana;
+
+        initDiesFeiners(diesFeiners);
+
+        assertTrue(diesFeiners.size() == 5);
+    }
+
+    public void initDiesFeiners(List<String> dies){
+        dies.add("dilluns");
+        dies.add("dimarts");
+        dies.add("dimecres");
+        dies.add("dijous");
+        dies.add("divendres");
     }
 }
