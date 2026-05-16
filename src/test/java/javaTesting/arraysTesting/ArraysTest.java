fc5176748a6ffebd9a1bfcaf674f73c2bafc4eca
Ibuki95 <lucasenriquebarcelo@gmail.com>
2026-05-16 12:51:43 +0200

    S'ha creat un nou mètode per provar l'ordenació d'arrays amb un forEach i un assertTrue.

--- src/test/java/javaTesting/arraysTesting/ArraysTest.java
+++ src/test/java/javaTesting/arraysTesting/ArraysTest.java
@@ -1,7 +1,11 @@
 package javaTesting.arraysTesting;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ArraysTest {
     @Test
@@ -17,5 +21,16 @@ public class ArraysTest {
         }
 
         assertEquals(nombres , "zero|un|dos|tres|");
+    }
+
+    @Test
+    public void provesOrdeancioArray(){
+        String[] numerals = {"zero", "un", "dos", "tres"};
+
+        Arrays.sort(numerals);
+
+        for(int i = 0; i < numerals.length - 1; i++){
+            assertTrue(numerals[i].compareTo(numerals[i+1]) < 0);
+        }
     }
 }
