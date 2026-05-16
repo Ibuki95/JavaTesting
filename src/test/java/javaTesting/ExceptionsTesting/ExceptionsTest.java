bfab27976a8338ade66ae1ed08ef10deeb5e17d3
Ibuki95 <lucasenriquebarcelo@gmail.com>
2026-05-16 16:30:52 +0200

    S'ha creat un nou mètode amb un try catch per s'exepcio de null pointer.

--- src/test/java/javaTesting/ExceptionsTesting/ExceptionsTest.java
+++ src/test/java/javaTesting/ExceptionsTesting/ExceptionsTest.java
@@ -2,11 +2,30 @@ package javaTesting.ExceptionsTesting;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ExceptionsTest {
     @Test(expected = NullPointerException.class)
     public void NullPointerTest(){
         Integer edat = null;
 
-        String edatenLletres = edat.toString();
+        String edatEnLletres = edat.toString();
+
+        assertEquals("" + 18, edatEnLletres);
+    }
+
+    @Test
+    public void catchNullPointerTest(){
+        Integer edat = null;
+        String edatEnLletres;
+
+         try {
+             edatEnLletres = edat.toString();
+         } catch (NullPointerException npe){
+             edat = 18;
+             edatEnLletres = edat.toString();
+         }
+
+        assertEquals("" + 18, edatEnLletres);
     }
 }
