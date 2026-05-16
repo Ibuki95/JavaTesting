c7f7c726cac5660fa3bf08623a169ce608f642d6
Ibuki95 <lucasenriquebarcelo@gmail.com>
2026-05-16 17:02:06 +0200

    S'ha creat un nou mètode per assercions de JUnit.

--- src/test/java/javaTesting/ExplorantJUnit/ExplorantJUnitTest.java
+++ src/test/java/javaTesting/ExplorantJUnit/ExplorantJUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.*;
 import org.junit.rules.ExpectedException;
 
 import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+import static  org.junit.Assert.*;
 
 public class ExplorantJUnitTest {
     @Rule
@@ -49,5 +52,23 @@ public class ExplorantJUnitTest {
     @After
     public void executaDespresDeCadaTest(){
         System.out.println("Després d'executar qualsevol test, per a cada test !!!");
+    }
+
+    @Test
+    public void assercionsDeJUnit(){
+        assertEquals(5, 2+3);
+
+        assertFalse("false és false", false);
+        assertFalse(false);
+        assertFalse(3 > 5);
+
+        assertTrue(5 > 3);
+
+        int[] finsA10 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] primers10 = {2, 1, 3, 5, 4, 6, 8, 7, 9 ,10};
+
+        Arrays.sort(primers10);
+
+        assertArrayEquals(finsA10, primers10);
     }
 }
